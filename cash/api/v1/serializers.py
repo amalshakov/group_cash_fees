d28e72fddf3f5311e9e2b9d80997b7680d66abd0
Alexander Malshakov <89681115653@mail.ru>
2024-03-01 09:55:09 +0300

    Finish Payment Collect json

--- cash/api/v1/serializers.py
+++ cash/api/v1/serializers.py
@@ -1,5 +1,6 @@
 from collects.models import Collect, Payment
 from django.contrib.auth import get_user_model
+from django.db.models import F, Sum, Count
 from rest_framework import serializers
 
 User = get_user_model()
@@ -16,14 +17,51 @@ class UserSerializer(serializers.ModelSerializer):
 class PaymentSerializer(serializers.ModelSerializer):
     """."""
 
+    payer_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Payment
-        fields = ("id", "money")
+        fields = (
+            "id",
+            "money",
+            "date_time_payment",
+            "payer_name",
+        )
+
+    def get_payer_name(self, obj):
+        return obj.donating.get_full_name()
 
 
 class CollectSerializer(serializers.ModelSerializer):
     """."""
 
+    author_name = serializers.SerializerMethodField()
+    occasion = serializers.CharField(source="get_occasion_display")
+    current_money = serializers.SerializerMethodField()
+    current_people = serializers.SerializerMethodField()
+    payments = PaymentSerializer(many=True)
+
     class Meta:
         model = Collect
-        fields = ("id", "purpose")
+        fields = (
+            "id",
+            "author_name",
+            "name",
+            "occasion",
+            "description",
+            "purpose",
+            "logo",
+            "date_time_collect",
+            "current_money",
+            "current_people",
+            "payments",
+        )
+
+    def get_author_name(self, obj):
+        return obj.author.get_full_name()
+
+    def get_current_money(self, obj):
+        return obj.payments.aggregate(Sum("money"))["money__sum"]
+
+    def get_current_people(self, obj):
+        return obj.payments.all().values("donating_id").distinct().count()
